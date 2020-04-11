2a47cd33ce4a5f5519d614e77a0d76154f994b9a
arLong-98 <dhairya999pateriya@gmail.com>
2020-04-11 21:12:08 +0530

    small fixes

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -20,10 +20,10 @@ const Feed = ()=>(
                     </div>
                 </header>
 
-                <body className="post-body">
+                <div className="post-body">
                 <ReadMoreReact text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
                                 readMoreText={"..."}/>
-                </body>
+                </div>
 
                 <footer className="post-footer">
                     <FooterElements />
@@ -45,10 +45,10 @@ const Feed = ()=>(
                     </div>
                 </header>
 
-                <body className="post-body">
+                <div className="post-body">
                 <ReadMoreReact text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
                                 readMoreText={"..."}/>
-                </body>
+                </div>
 
                 <footer className="post-footer">
                     <FooterElements />
@@ -69,10 +69,10 @@ const Feed = ()=>(
                     </div>
                 </header>
 
-                <body className="post-body">
+                <div className="post-body">
                 <ReadMoreReact text={"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."}
                                 readMoreText={"..."}/>
-                </body>
+                </div>
 
                 <footer className="post-footer">
                     <FooterElements />
